4c065954869916cd27fa1025c4194de178b14d19
bmihetu <motta4259@gmail.com>
2019-07-18 19:51:43 -0400

    changes in controller, and form html

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -4,10 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -25,22 +22,24 @@ public class HomeController {
 
     @GetMapping("/add")
     public String newMessage(Model model){
-        model.addAttribute("message", new Bullhorn());
+        Bullhorn bull = new Bullhorn();
+        model.addAttribute("bullhorn", bull);
         return "messageForm";
     }
 
     @PostMapping("/process")
-    public String processMessage(@Valid Bullhorn message, BindingResult result){
+    public String processMessage(@Valid Bullhorn bullhorn, Model model, BindingResult result){
         if(result.hasErrors()){
             return "messageForm";
         }
-        messageRepository.save(message);
+        model.addAttribute("bullhorn", bullhorn);
+        messageRepository.save(bullhorn);
         return "redirect:/";
     }
 
     @RequestMapping("/update/{id}")
     public String updateMessage(@PathVariable("id") long id, Model model){
-        model.addAttribute("message", messageRepository.findById(id).get());
+        model.addAttribute("bullhorn", messageRepository.findById(id).get());
         return "messageForm";
     }
 
